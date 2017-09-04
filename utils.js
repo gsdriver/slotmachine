f74ce8a2ac4ea7066f029a1360577ae1d933a1c4
gsdriver <gsdriver@live.com>
2017-09-04 07:10:43 -0700

    Async call

--- utils.js
+++ utils.js
@@ -118,27 +118,22 @@ module.exports = {
         if (err) {
           console.log(err);
         }
-        completed();
       });
-    } else {
-      completed();
     }
 
-    function completed() {
-      if (!process.env.NOLOG) {
-        console.log(JSON.stringify(globalEvent));
-      }
-      if (error) {
-        const res = require('./' + locale + '/resources');
-        console.log('Speech error: ' + error);
-        emit(':ask', error, res.ERROR_REPROMPT);
-      } else if (response) {
-        emit(':tell', response);
-      } else if (cardTitle) {
-        emit(':askWithCard', speech, reprompt, cardTitle, cardText);
-      } else {
-        emit(':ask', speech, reprompt);
-      }
+    if (!process.env.NOLOG) {
+      console.log(JSON.stringify(globalEvent));
+    }
+    if (error) {
+      const res = require('./' + locale + '/resources');
+      console.log('Speech error: ' + error);
+      emit(':ask', error, res.ERROR_REPROMPT);
+    } else if (response) {
+      emit(':tell', response);
+    } else if (cardTitle) {
+      emit(':askWithCard', speech, reprompt, cardTitle, cardText);
+    } else {
+      emit(':ask', speech, reprompt);
     }
   },
   setEvent: function(event) {
