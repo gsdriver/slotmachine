86e24384dbdde78e56e41d6e56c6dc1e593dfe98
gsdriver <gsdriver@live.com>
2018-05-07 06:28:36 -0700

    Meat symbols

--- intents/Stop.js
+++ intents/Stop.js
@@ -18,25 +18,8 @@ module.exports = {
       return;
     }
 
-    const tournamentTime = utils.timeUntilTournament();
-    let speech;
-
-    if (tournamentTime && (tournamentTime.days || tournamentTime.hours)) {
-      if (tournamentTime.days == 0) {
-        // It's later today
-        speech = res.strings.EXIT_TOURNAMENT_TODAY;
-      } else if (tournamentTime.days < 3) {
-        // Tease it!
-        speech = res.strings.EXIT_TOURNAMENT_SOON;
-      }
-    }
-
-    if (speech) {
-      utils.emitResponse(this, null, speech, null, null);
-    } else {
-      ads.getAd(this.attributes, 'slots', this.event.request.locale, (adText) => {
-        utils.emitResponse(this, null, res.strings.EXIT_GAME.replace('{0}', adText), null, null);
-      });
-    }
+    ads.getAd(this.attributes, 'slots', this.event.request.locale, (adText) => {
+      utils.emitResponse(this, null, res.strings.EXIT_GAME.replace('{0}', adText), null, null);
+    });
   },
 };
