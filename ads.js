02b9af80236838aa29c0a53b841cd8a4d60e96f6
Garrett Vargas <gsdriver@live.com>
2018-05-26 09:32:24 -0700

    New ad framework

--- ads.js
+++ ads.js
@@ -14,9 +14,22 @@ module.exports = {
     if (process.env.NOAD) {
       callback('');
     } else {
-      // Read the S3 buckets that has everyone's scores
-      const adBucket = 'garrett-alexa-usage/ads/' + skill + '/' + locale;
+      // First reformat the ads played if it's old style
+      if (attributes.adsPlayed) {
+        let ad;
+        for (ad in attributes.adsPlayed) {
+          if (!attributes.adsPlayed[ad].hasOwnProperty('timesPlayed')) {
+            const newAd = {};
+            newAd.time = attributes.adsPlayed[ad];
+            newAd.timesPlayed = 1;
+            attributes.adsPlayed[ad] = newAd;
+          }
+        }
+      }
 
+      // Read the S3 buckets that has everyone's scores
+      const languages = locale.split('-');
+      const adBucket = 'garrett-alexa-usage/ads/' + skill;
       s3.getObject({Bucket: adBucket, Key: 'CurrentAds.txt'}, (err, data) => {
         if (err) {
           console.log('Error reading current ad list: ' + err);
@@ -25,21 +38,49 @@ module.exports = {
           // OK, let's parse the current ads and see which we've already run
           const ads = JSON.parse(data.Body.toString('ascii'));
           let adToRun;
+          let adRepeats;
 
           if (!ads || !ads.running) {
             callback('');
           } else {
             ads.running.forEach((ad) => {
-              // Did we already show this ad?  If not, that's the one we'll pick
-              if (!adToRun && (!attributes.adsPlayed || !attributes.adsPlayed[ad.name])) {
-                // If there is a condition, make sure that it's met
-                if (ad.condition) {
-                  if (eval(ad.condition)) {
-                    adToRun = ad.name;
+              // Is this ad appropriate for this locale (no locale means all locales)
+              let runInLocale;
+              if (ad.locale) {
+                const localesToRun = ad.locale.split('|');
+                localesToRun.forEach((loc) => {
+                  if ((loc == locale) || (loc == languages[0])) {
+                    runInLocale = true;
                   }
-                } else {
-                  // No condition - just run this
-                  adToRun = ad.name;
+                });
+              } else {
+                // OK to run globally
+                runInLocale = true;
+              }
+
+              if (runInLocale) {
+                // Did we already show this ad?  If not, that's the one we'll pick
+                if (!adToRun) {
+                  // If there is a condition, make sure that it's met
+                  let adCandidate;
+                  if (ad.condition) {
+                    if (eval(ad.condition)) {
+                      adCandidate = ad.name;
+                      adRepeats = (ad.repeat) ? ad.repeat : 1;
+                    }
+                  } else {
+                    // No condition - just run this
+                    adCandidate = ad.name;
+                    adRepeats = (ad.repeat) ? ad.repeat : 1;
+                  }
+
+                  // Make sure we haven't run this ad the requested number of times
+                  if (adCandidate && (!attributes.adsPlayed || !attributes.adsPlayed[adCandidate]
+                    || (adRepeats === 'infinity')
+                    || (attributes.adsPlayed[adCandidate].timesPlayed < adRepeats))) {
+                    // We're good with this one!
+                    adToRun = adCandidate;
+                  }
                 }
               }
             });
@@ -56,7 +97,12 @@ module.exports = {
                   if (!attributes.adsPlayed) {
                     attributes.adsPlayed = {};
                   }
-                  attributes.adsPlayed[adToRun] = Date.now();
+                  if (!attributes.adsPlayed[adToRun]) {
+                    attributes.adsPlayed[adToRun] = {};
+                  }
+                  attributes.adsPlayed[adToRun].time = Date.now();
+                  attributes.adsPlayed[adToRun].timesPlayed =
+                    (attributes.adsPlayed[adToRun].timesPlayed + 1) || 1;
                   callback(adText ? adText : '');
                 }
               });
