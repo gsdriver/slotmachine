4907c71a3ac80a1e805a8c1ee7178b5ce8fdf930
Garrett Vargas <gsdriver@live.com>
2018-07-29 18:45:58 -0700

    Use SNS to mail purchase response

--- lambda/custom/intents/ProductResponse.js
+++ lambda/custom/intents/ProductResponse.js
@@ -5,6 +5,9 @@
 'use strict';
 
 const Launch = require('./Launch');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+const SNS = new AWS.SNS();
 
 module.exports = {
   canHandle: function(handlerInput) {
@@ -15,17 +18,38 @@ module.exports = {
     const event = handlerInput.requestEnvelope;
     const attributes = handlerInput.attributesManager.getSessionAttributes();
 
-    // Launch processing will handle updating the bankroll as necessary
-    // We just need to check if they declined an upsell request
-    // to avoid an infinite loop
-    console.log('Response is ' + event.request.name);
-    if ((event.request.name === 'Upsell') &&
-      !(event.request.payload &&
-        ((event.request.payload.purchaseResult == 'ACCEPTED') ||
-         (event.request.payload.purchaseResult == 'ALREADY_PURCHASED')))) {
-      attributes.temp.noUpsell = true;
+    // Publish to SNS so we know something happened
+    if (process.env.SNSTOPIC) {
+      const start = Date.now();
+      SNS.publish({
+        Message: event.request.name + ' was ' + event.request.payload.purchaseResult
+          + ' by user ' + event.session.user.userId,
+        TopicArn: process.env.SNSTOPIC,
+        Subject: 'Slot Machine Purchase Response',
+      }, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('SNS post took ' + (Date.now() - start) + ' ms');
+        done();
+      });
+    } else {
+      done();
     }
 
-    Launch.handle(handlerInput);
+    function done() {
+      // Launch processing will handle updating the bankroll as necessary
+      // We just need to check if they declined an upsell request
+      // to avoid an infinite loop
+      console.log('Response is ' + event.request.name);
+      if ((event.request.name === 'Upsell') &&
+        !(event.request.payload &&
+          ((event.request.payload.purchaseResult == 'ACCEPTED') ||
+           (event.request.payload.purchaseResult == 'ALREADY_PURCHASED')))) {
+        attributes.temp.noUpsell = true;
+      }
+
+      Launch.handle(handlerInput);
+    }
   },
 };
