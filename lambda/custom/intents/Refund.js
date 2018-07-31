426a8626f76708c7d35d03fe3b42202d51927150
gsdriver <gsdriver@live.com>
2018-07-30 23:08:52 -0700

    No need for extra refund prompt

--- lambda/custom/intents/Refund.js
+++ lambda/custom/intents/Refund.js
@@ -11,39 +11,15 @@ module.exports = {
     const request = handlerInput.requestEnvelope.request;
     const attributes = handlerInput.attributesManager.getSessionAttributes();
 
-    if ((request.type === 'IntentRequest') && attributes.paid && attributes.paid.coins
-      && ((!attributes.temp.refunding && (request.intent.name === 'RefundIntent')) ||
-       (attributes.temp.refunding &&
-        ((request.intent.name === 'AMAZON.YesIntent') || (request.intent.name === 'AMAZON.NoIntent'))))) {
-      return true;
-    }
-
-    attributes.temp.refunding = undefined;
-    return false;
+    return ((request.type === 'IntentRequest') && attributes.paid && attributes.paid.coins
+      && (attributes.paid.coins.state == 'PURCHASED')
+      && (request.intent.name === 'RefundIntent'));
   },
   handle: function(handlerInput) {
-    const event = handlerInput.requestEnvelope;
     const attributes = handlerInput.attributesManager.getSessionAttributes();
-    const res = require('../resources')(event.request.locale);
 
-    if (attributes.temp.refunding) {
-      // In the middle of refunding
-      if (event.request.intent.name === 'AMAZON.YesIntent') {
-        handlerInput.responseBuilder
-          .addDirective(utils.getPurchaseDirective(attributes, 'Cancel'))
-          .withShouldEndSession(true);
-      } else {
-        // They don't want to cancel - just repeat
-        attributes.temp.refunding = undefined;
-        handlerInput.responseBuilder
-          .speak(res.strings.REFUND_NO_REFUND)
-          .reprompt(res.strings.REFUND_NO_REFUND);
-      }
-    } else {
-      attributes.temp.refunding = true;
-      handlerInput.responseBuilder
-        .speak(res.strings.REFUND_RESETBANKROLL)
-        .reprompt(res.strings.REFUND_CONFIRM_REPROMPT);
-    }
+    handlerInput.responseBuilder
+      .addDirective(utils.getPurchaseDirective(attributes, 'Cancel'))
+      .withShouldEndSession(true);
   },
 };
